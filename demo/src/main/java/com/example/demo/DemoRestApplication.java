aea6e36c0d1867bb71502810eaa74f4886730893
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2021-05-15 17:34:37 +0430

    micro profile

---
+++ demo/src/main/java/com/example/demo/DemoRestApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+
+
+
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class DemoRestApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+
+        Set<Class<?>> classes = new HashSet<>();
+
+        // resources
+        classes.add(HelloController.class);
+        
+        
+        
+        
+        
+        return classes;
+    }
+}
